c8bf615cdba8f4460a2781ee3487f85516a6c9bb
nettredaksjonenTrondheimKommune <nettredaksjonen@trondheim.kommune.no>
2021-06-18 11:52:04 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Grid,
+    Card,
+    CardHeader,
+    CardContent,
+    Typography,
+    CircularProgress
+} from '@material-ui/core/';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    minWidth: 250
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  barColorGreen: {
+    color: "green",
+  },
+  barColorOrange: {
+    color: "orange"
+  },
+  barColorRed: {
+    color: "red"
+  }
+});
+
+export default function App() {
+  const classes = useStyles();
+  const json = require('./data.json');
+  const requestedUrl = "Status for: " + json.requestedUrl.slice(0, -1);
+  const data = [json.categories["accessibility"], json.categories["best-practices"], json.categories["performance"], json.categories["seo"]];
+
+  let cards = data.map(res => ({
+    id: res.id,
+    title: res.title,
+    score: res.score * 100,
+    barColor: ''
+  }));
+
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].score > 75) {
+      cards[i].barColor = classes.barColorGreen;
+    } else if (cards[i].score < 75 && cards[i].score > 25) {
+      cards[i].barColor = classes.barColorOrange;
+    } else {
+      cards[i].barColor = classes.barColorRed;
+    }
+  }
+
+  const cardContent =
+    (<Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center">
+            <Grid item>
+              <Card>
+                <CardHeader title={requestedUrl} />
+                {cards.map((card) => (
+                  <CardContent key={card.id}>
+                    <Typography variant="h5" component="h2">
+                      {card.title}
+                    </Typography>
+                    <Typography variant="body1" component="h3" color="textSecondary">
+                      <CircularProgress className={card.barColor} variant="determinate" value={card.score} />
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      Score: {card.score}
+                    </Typography>
+                  </CardContent>
+                ))}
+              </Card>
+            </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cardContent}
     </div>
   );
 }
-
-export default App;
